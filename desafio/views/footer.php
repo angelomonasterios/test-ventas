dafb54103c7fa41c9e6c3ea520b1d65bd7a3f53b
angelomonasterios <49825664+angelomonasterios@users.noreply.github.com>
2020-01-27 13:07:56 -0300

    Update footer.php

--- desafio/views/footer.php
+++ desafio/views/footer.php
@@ -11,6 +11,6 @@
 
 <script src="<?=base_url?>/assets/js/bootstrap.min.js" ></script>
 <script src="<?=base_url?>/assets/js/jquery.js"></script>
-<script src="<?=base_url?>/assets/js/script.js"></script>
+
 </body>
 </html>
