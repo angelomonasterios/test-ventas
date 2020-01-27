06902a1b73d1cfbe220c593a2072cec3384b3ada
angelomonasterios <49825664+angelomonasterios@users.noreply.github.com>
2020-01-27 13:07:17 -0300

    Update header.php

--- desafio/views/header.php
+++ desafio/views/header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Tienda de camisas</title>
+    <title>test ventas</title>
 
     <link rel="stylesheet" href="<?=base_url?>assets/css/styles.css">
     <link rel="stylesheet" href="<?=base_url?>assets/css/bootstrap.css">
